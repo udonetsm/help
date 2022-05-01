e1a669257904f2be3ae1fa7eb46c3cf02d43f4ed
udonetsm <donetsmaksimm@yandex.ru>
2022-05-01 14:05:08 +0300

    fix redis package

--- redis/redis.go
+++ redis/redis.go
@@ -3,30 +3,30 @@ package redis
 import (
 	"github.com/go-redis/redis"
 	"github.com/udonetsm/help/helper"
-	"github.com/udonetsm/help/models"
 )
 
 func NewStore(opts *redis.Options) *redis.Client {
 	return redis.NewClient(opts)
 }
 
-func NewOpts(username, password string) *redis.Options {
+func NewOpts(username, password string, db int) *redis.Options {
 	return &redis.Options{
 		Addr:     ":6379",
-		Password: "",
-		DB:       0,
+		Password: password,
+		Username: username,
+		DB:       db,
 	}
 }
 
-func SetVals(key, value string, auth models.Auth) error {
-	store := NewStore(NewOpts(auth.Email, auth.Password))
+func SetVals(key, value, dbusername, dbpassword string, db int) error {
+	store := NewStore(NewOpts(dbusername, dbpassword, db))
 	err := store.Set(store.Context(), key, value, 0).Err()
 	helper.Errors(err, "set(setvals)")
 	return err
 }
 
-func GetVals(key string, auth models.Auth) interface{} {
-	store := NewStore(NewOpts(auth.Email, auth.Password))
+func GetVals(key, dbusername, dbpassword string, db int) interface{} {
+	store := NewStore(NewOpts(dbusername, dbpassword, db))
 	vals, err := store.Get(store.Context(), key).Result()
 	helper.Errors(err, "get(getvals)")
 	return vals
