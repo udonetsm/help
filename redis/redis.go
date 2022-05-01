73eceea28097b477063f31c74316258caa30601c
udonetsm <donetsmaksimm@yandex.ru>
2022-05-01 13:25:26 +0300

    add redis function

---
+++ redis/redis.go
@@ -0,0 +1,35 @@
+package redis
+
+import (
+	"github.com/go-redis/redis"
+
+	"github.com/udonetsm/help/helper"
+	"github.com/udonetsm/help/models"
+)
+
+func NewStore(opts *redis.Options) *redis.Client {
+	return redis.NewClient(opts)
+}
+
+func NewOpts(username, password string) *redis.Options {
+	return &redis.Options{
+		Addr:     ":6379",
+		Password: password,
+		Username: username,
+		DB:       0,
+	}
+}
+
+func SetVals(key, value string, auth models.Auth) error {
+	store := NewStore(NewOpts(auth.Email, auth.Password))
+	err := store.Set(store.Context(), key, value, 0).Err()
+	helper.Errors(err, "set(setvals)")
+	return err
+}
+
+func GetVals(key string, auth models.Auth) interface{} {
+	store := NewStore(NewOpts(auth.Email, auth.Password))
+	vals, err := store.Get(store.Context(), key).Result()
+	helper.Errors(err, "get(getvals)")
+	return vals
+}
